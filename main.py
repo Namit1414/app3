79dcebe3ac88e5e415c0f7e9ae6c5a8de42979c0
Namit1414 <66672652+Namit1414@users.noreply.github.com>
2020-06-17 19:03:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from kivy.utils import get_color_from_hex
+from kivymd.uix.expansionpanel import MDExpansionPanel
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDIconButton, MDRaisedButton
 from kivy.lang import Builder
-from Helpers import KV
+from kivymd.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.toast import toast
 from kivy.core.window import Window
+from playsound import playsound
+
 
 
 class MainWindow(Screen):
@@ -39,39 +41,51 @@ class WindowManager(ScreenManager):
 
 class MainApp(MDApp):
     def build(self):
-        #Window.clearcolor = (0, 0, 0, 1)
+        # Window.clearcolor = (0, 0, 0, 1)
         # self.theme_cls.primary_palette = "Blue"
         # self.theme_cls.theme_style = 'Dark'
         return
+
+
+
     def dialog(self):
-        butt= MDRaisedButton(text="AB YAHAN CLICK KAR",on_release=self.dialog2)
+        butt = MDRaisedButton(text="AB YAHAN CLICK KAR", on_release=self.dialog2,pos_hint={'center_x': 0.5, 'center_y': 0.4})
         self.dialog10 = MDDialog(title='Mana Kara Tha Na',
-                               text=" Sun Liya Kar Baat "
-                                    "Ab Neeche Click Kar"
-                                    " Aur Iss Baar Sirf Upar Wale Cake Ko Touch Kariyo"
-                               , size_hint=(0.7, 1), auto_dismiss='FALSE',buttons=[butt])
+                                 text=" Sun Liya Kar Baat "
+                                      "Ab Neeche Click Kar"
+                                      " Aur Iss Baar Sirf Upar Wale Cake Ko Touch Kariyo"
+                                 , size_hint=(0.7, 1), auto_dismiss='FALSE', buttons=[butt])
 
         self.dialog10.open()
         pass
+
     def dialog2(self, obj):
         self.dialog10.dismiss()
         pass
 
-
     def show_MDDialog(self):
         self.theme_cls.primary_palette = "Amber"
-        button1 = MDRaisedButton(text='ORANGE', font_size=(15), on_release=self.close_dialog)
-        button2 = MDRaisedButton(text='THANK YOU', font_size=(15), on_release=self.change_color)
-        button3 = MDIconButton(icon="close", user_font_size="23sp", on_release=self.close_dialog2)
+        button1 = MDRaisedButton(text='ORANGE', font_size=(15), on_release=self.close_dialog,pos_hint={'center_x': 0.5, 'center_y': 0.4})
+        button2 = MDRaisedButton(text='THANK YOU', font_size=(15), on_release=self.change_color,pos_hint={'center_x': 0.5, 'center_y': 0.4})
+        button3 = MDIconButton(icon="close", user_font_size="23sp", on_release=self.close_dialog2,pos_hint={'center_x': 0.5, 'center_y': 0.4})
         self.dialog = MDDialog(title='HAPPY BIRTHDAY BRUHHHHHHH',
-                               text="IT HAS BEEN AN AWESOME "
-                                    "JOURNEY AND WILL ALWAYS BE ONE"
-                                    " THANK YOU FOR ALWAYS BEING THERE FOR ME"
-                               , size_hint=(0.7, 1), auto_dismiss='FALSE',
+                               text="IT HAS BEEN AN AWESOME"
+                                    " JOURNEY AND "
+                                    "WILL ALWAYS BE ONE"
+                                    " THANK YOU FOR "
+                                    "ALWAYS BEING "
+                                    "THERE FOR ME", size_hint=(0.9, 1),pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                               auto_dismiss='FALSE',
                                buttons=[button3, button1, button2])
 
         self.dialog.open()
 
+    def call(self):
+        toast('HAPPY BIRTHDAY BROOO')
+        pass
+    def audio(self):
+        playsound('thank.mp3')
+        pass
     def close_dialog(self, obj):
         toast('Close card')
         pass
